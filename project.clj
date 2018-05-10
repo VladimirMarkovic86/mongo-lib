4d97bae636f11e4b1374c5d8059e8338d51f03e7
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-05-10 14:57:13 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject org.vladimir/mongo-lib "0.1.0"
+  :description "Mongo library"
+  :url "http://gitlab:1610/VladimirMarkovic86/mongo-lib"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure			   "1.9.0"]
+         									;	https://clojure.org/api/api
+         								[org.mongodb/mongo-java-driver "3.6.3"]
+         								]
+  :source-paths ["src/clj"])
