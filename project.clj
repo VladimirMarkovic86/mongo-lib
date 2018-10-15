328101df6b0e8a80cc8092ce03f1eed840cea58a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:46:10 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,9 +1,13 @@
-(defproject org.vladimir/mongo-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/mongo-lib "0.1.0"
   :description "Mongo library"
-  :url "http://gitlab:1610/VladimirMarkovic86/mongo-lib"
+  :url "http://github.com/VladimirMarkovic86/mongo-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.mongodb/mongo-java-driver "3.6.3"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"])
+
