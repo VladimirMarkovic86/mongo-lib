2bbb10a70edff1a79de166a6cf9f6a158f471f5e
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-12 17:53:38 +0200

    Task 88: mongo-lib now has dependency org.mongodb/mongodb-driver 3.9.0 because of Duplicate class exception in android studio.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/mongo-lib "0.2.9"
+(defproject org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"
   :description "Mongo library"
   :url "http://github.com/VladimirMarkovic86/mongo-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.mongodb/mongo-java-driver "3.9.0"]
+                 [org.mongodb/mongodb-driver "3.9.0"]
                  ]
 
   :min-lein-version "2.0.0"
