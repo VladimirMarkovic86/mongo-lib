677797a9a18678acc5b0d5b7eb3126a462933c02
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:54:13 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"
+(defproject org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"
   :description "Mongo library"
   :url "http://github.com/VladimirMarkovic86/mongo-lib"
   :license {:name "Eclipse Public License"
@@ -10,5 +10,8 @@
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"])
+  :test-paths ["test/clj"]
+  
+  :jar-exclusions [#"README.md$"
+                   #"LICENSE$"])
 
